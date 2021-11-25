05334e897439a3e7f1957823460b217775fc719f
navid.k(김진곤)/kakao <navid.k@kakaocorp.com>
2021-11-25 19:13:32 +0900

    baba

--- main.py
+++ main.py
@@ -5,10 +5,9 @@
 
 
 def print_hi(name):
+    print("a")
 
 
-# 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
-# https://www.jetbrains.com/help/pycharm/에서 PyCharm 도움말 참조
+# https://www.jetbrainsdd.com/help/pycharm/에서 PyCharm 도움말 참조
